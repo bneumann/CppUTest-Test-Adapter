80e99550a5c75586263b44ff274d80838f35512a
Benjamin Giesinger <benjamin.giesinger@gmail.com>
2020-11-05 22:02:35 +0100

    Removed the XML parsing by fancy regexp magic

--- src/cpputest.ts
+++ src/cpputest.ts
@@ -1,9 +1,7 @@
 import * as vscode from 'vscode';
 import { exec, execFile, ChildProcess } from 'child_process';
-import { TestSuiteInfo, TestInfo, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent, TestDecoration } from 'vscode-test-adapter-api';
+import { TestSuiteInfo, TestInfo, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
 import { CppUTest, CppUTestGroup } from './CppUTestImplementation';
-import * as xml2js from 'xml2js';
-import * as fs from 'fs';
 import * as pathModule from 'path';
 
 let suite: CppUTestGroup;
@@ -29,11 +27,9 @@ export function loadTests(): Promise<TestSuiteInfo> {
             suite.children.push(subSuite);
             groups.forEach(g => subSuite.children.push(new CppUTestGroup(g)));
 
-            for(let i: number = 0; i < groupStrings.length; i++)
-            {
+            for (let i: number = 0; i < groupStrings.length; i++) {
                 const gs = groupStrings[i];
-                for(let j: number = 0; j < subSuite.TestGroups.length; j++)
-                {
+                for (let j: number = 0; j < subSuite.TestGroups.length; j++) {
                     const tg: CppUTestGroup = subSuite.TestGroups[j];
                     const group = gs.split(".")[0];
                     const test = gs.split(".")[1];
@@ -51,22 +47,21 @@ export function loadTests(): Promise<TestSuiteInfo> {
     return Promise.resolve<TestSuiteInfo>(promise);
 }
 
-async function getLineAndFile(command: string, path: string, group: string, test: string): Promise<{file?: string, line?: number}>
-{
+async function getLineAndFile(command: string, path: string, group: string, test: string): Promise<{ file?: string, line?: number }> {
     // Linux:
     const sourceGrep = `objdump -lSd ${command} | grep -m 1 -A 2 TEST_${group}_${test}`;
     // Windows:
     // const sourceGrep = `windObjDumpOrWhatEver ${command} | findstr TEST_${group}_${test}`
-    return new Promise<{file?: string, line?: number}>((resolve, reject) => {
+    return new Promise<{ file?: string, line?: number }>((resolve, reject) => {
         exec(sourceGrep, { cwd: path }, (error, stdout, stderr) => {
             if (error) {
                 console.error('stderr', error.cmd);
-                resolve({file: undefined, line: undefined});
+                resolve({ file: undefined, line: undefined });
             } else {
                 const debugSymbols: string[] = stdout.split("\n")[2].split(":");
                 const file = debugSymbols[0];
                 const line = parseInt(debugSymbols[1], 10) - 2; // show it above the test
-                resolve({file, line});
+                resolve({ file, line });
             }
         });
     })
@@ -90,8 +85,7 @@ export function killTestRun() {
 
 export async function debugTest(tests: string[]) {
     const config = getDebugConfiguration();
-    if(config === "")
-    {
+    if (config === "") {
         throw new Error("No debug configuration found. Not able to debug!");
     }
     for (const suiteOrTestId of tests) {
@@ -100,8 +94,7 @@ export async function debugTest(tests: string[]) {
             (config as any).name = node.id;
             const arg: string = node.id.search(/\./) >= 0 ? "-t" : "-sg";
             (config as any).args = [arg, node.id];
-            if(vscode.workspace.workspaceFolders)
-            {
+            if (vscode.workspace.workspaceFolders) {
                 await vscode.debug.startDebugging(vscode.workspace.workspaceFolders[0], config);
             }
         }
@@ -152,79 +145,42 @@ async function runNode(
 
 async function runSingleCall(command: string, group: string, test: string, path: string) {
     const promise: Promise<TestEvent> = new Promise<TestEvent>((resolve, reject) => {
-        const runProcess: ChildProcess = execFile(command, ["-sg", group, "-sn", test, "-ojunit"], { cwd: path }, (error: any, stdout, stderr) => {
+        const runProcess: ChildProcess = execFile(command, ["-sg", group, "-sn", test, "-v"], { cwd: path }, (error: any, stdout, stderr) => {
             if (error && error.code === null) {
                 resolve(Promise.resolve(<TestEvent>{ type: 'test', test: suite.findTest(group + "." + test), state: 'errored', message: stderr }));
                 return;
             }
-            resolve(evaluateXML(group, path));
+            const regexPattern: RegExp = /(\w*)_*TEST\((\w*), (\w*)\)(.*?)- (\d*) ms/gs;
+            const result: RegExpExecArray | null = regexPattern.exec(stdout)
+            if (result == null) {
+                reject({ "reason": "unkown" });
+            }
+            else {
+                let state: TestEvent["state"] = "passed";
+                if (result[1] == "IGNORE_") {
+                    state = "skipped";
+                }
+                if(result[4].trim())
+                {
+                    state = "failed";
+                }
+                console.log(result);
+                const event: TestEvent = {
+                    type: 'test',
+                    test: new CppUTest(test, group),
+                    state: state,
+                    message: result[4].trim(),
+                    decorations: undefined
+                }
+                console.log(event);
+                resolve(event);
+            }
         });
         processes.push(runProcess);
     });
     return Promise.resolve(promise);
 }
 
-async function evaluateXML(
-    group: string,
-    path: string) {
-    const parser: xml2js.Parser = new xml2js.Parser();
-    path = path == "" ? "." : path;
-    const fileName: string = path + "/cpputest_" + group + ".xml";
-    if (!fs.existsSync(fileName)) {
-        return Promise.resolve(<TestEvent>{ type: 'test', test: group, state: 'errored', message: "Test crashed" })
-    }
-    const xml: Buffer = fs.readFileSync(fileName);
-    const promise: Promise<TestEvent> = new Promise<TestEvent>((resolve, reject) => {
-        parser.parseString(xml, function (err: string, result: any) {
-            if (err) {
-                reject(err);
-            } else {
-                const tc: any = result.testsuite.testcase[0];
-                const testName: string = tc.$.name;
-                const testGroup: string = tc.$.classname;
-                const testFile: string = tc.$.file;
-                const testLine: number = Number.parseInt(tc.$.line);
-                let state: TestEvent["state"];
-                if (tc.failure) {
-                    state = "failed";
-                }
-                else if (tc.skipped) {
-                    state = "skipped";
-                }
-                else {
-                    state = "passed";
-                }
-                let message: string = "";
-                let decoration: TestDecoration[] | undefined = undefined;
-                if (state === "failed") {
-                    const failure: any = tc.failure[0].$;
-                    message = failure.message.replace(/\{newline\}/g, "\n");
-                    decoration = [
-                        {
-                            line: Number.parseInt(message.split(":")[1]),
-                            message: message.split(":")[2]
-                        }
-                    ];
-                }
-
-                const testInfo = new CppUTest(testName, testGroup);
-                const path: string = getTestPath();
-                testInfo.file = path + testFile;
-                testInfo.line = testLine;
-                suite.updateTest(testInfo);
-                const event: TestEvent = {
-                    type: 'test',
-                    test: testInfo,
-                    state: state,
-                    message: message,
-                    decorations: decoration
-                }
-                resolve(event);
-            }
-        });
-    })
-    return Promise.resolve(promise);
-}
 
 export function getTestRunner(): string {
     const runner: string | undefined = vscode.workspace.getConfiguration("cpputestExplorer").testExecutable;
@@ -278,10 +234,14 @@ function getDebugConfiguration(): (vscode.DebugConfiguration | string) {
     return "";
 }
 
-function IsCCppDebugger(config: any){
+function IsCCppDebugger(config: any) {
+    const isWin = process.platform === "win32";
+    // This is my way of saying: If we are using windows check for a config that has an .exe program.
+    const executionExtension: boolean = isWin ? config.program.endsWith(".exe") : true;
     return config.request == 'launch' &&
         typeof config.type == 'string' &&
+        executionExtension &&
         (config.type.startsWith('cpp') ||
-         config.type.startsWith('lldb') ||
-         config.type.startsWith('gdb'));
+            config.type.startsWith('lldb') ||
+            config.type.startsWith('gdb'));
 }
