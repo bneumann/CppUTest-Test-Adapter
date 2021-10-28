d65dc07a423b64d11059a028b5a75c89052a9e08
Benjamin Giesinger <benjamin.giesinger@pm.me>
2021-10-28 15:19:18 +0200

    Patched some caching into the runner

--- src/Infrastructure/ExecutableRunner.ts
+++ src/Infrastructure/ExecutableRunner.ts
@@ -8,7 +8,9 @@ export default class ExecutableRunner {
   private readonly kill: Function;
   private readonly command: string;
   private readonly workingDirectory: string;
+  private readonly tempFile: string;
   public readonly Name: string;
+  private dumpCached: boolean;
 
   constructor(processExecuter: ProcessExecuter, command: string, workingDirectory: string = dirname(command)) {
     this.exec = processExecuter.Exec;
@@ -17,6 +19,8 @@ export default class ExecutableRunner {
     this.command = command;
     this.workingDirectory = workingDirectory;
     this.Name = basename(command);
+    this.tempFile = `${this.Name}.dump`
+    this.dumpCached = false;
   }
 
   public get Command(): string { return this.command; }
@@ -33,17 +37,37 @@ export default class ExecutableRunner {
 
   // TODO: Make this abstract in case windows will be required
   public GetDebugSymbols(group: string, test: string): Promise<string> {
+    const sourceGrep = `grep -m 2 -A 2 TEST_${group}_${test} ${this.tempFile}`;
+    // Windows:
+    // const sourceGrep = `findstr TEST_${group}_${test}`
+    return this.DumpSymbols().then(() => {
+      return new Promise<string>((resolve, reject) => {
+        this.exec(sourceGrep, { cwd: this.workingDirectory }, (error: ExecException | null, stdout: string, stderr: string) => {
+          if (error) {
+            console.error('stderr', error);
+            reject(stderr);
+          } else {
+            resolve(stdout);
+          }
+        });
+      });
+    })
+  }
+
+  private DumpSymbols(): Promise<void> {
+    if (this.dumpCached) { return Promise.resolve(); }
     // Linux:
-    const sourceGrep = `objdump -lSd ${this.command} | grep -m 2 -A 2 TEST_${group}_${test}`;
+    const sourceGrep = `objdump -lSd ${this.command} > ${this.tempFile}`;
     // Windows:
     // const sourceGrep = `windObjDumpOrWhatEver ${command} | findstr TEST_${group}_${test}`
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       this.exec(sourceGrep, { cwd: this.workingDirectory }, (error: ExecException | null, stdout: string, stderr: string) => {
         if (error) {
           console.error('stderr', error);
           reject(stderr);
         } else {
-          resolve(stdout);
+          this.dumpCached = true;
+          resolve();
         }
       });
     });
